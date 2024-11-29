f4d5b133a72f24ec036858cfb64edb2dacb7a300
AssiaIlieva <ac17@abv.bg>
2024-11-29 17:59:27 +0200

    Update Catalogue Component

--- shared-cookbook/src/app/app.component.ts
+++ shared-cookbook/src/app/app.component.ts
@@ -1,18 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './core/header/header.component';
-import { RecipeCardComponent } from './recipes/recipe-card/recipe-card.component';
 import { CatalogComponent } from './recipes/catalog/catalog.component';
+import { LoaderComponent } from './shared/loader/loader.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    HeaderComponent,
-    RecipeCardComponent,
-    CatalogComponent,
-  ],
+  imports: [RouterOutlet, HeaderComponent, CatalogComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- shared-cookbook/src/app/recipes/catalog/catalog.component.ts
+++ shared-cookbook/src/app/recipes/catalog/catalog.component.ts
@@ -1,10 +1,12 @@
 import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { ApiService } from '../../api.service';
+import { Recipe } from '../recipe.model';
+import { LoaderComponent } from '../../shared/loader/loader.component';
 
 @Component({
   selector: 'app-catalog',
   standalone: true,
-  imports: [],
+  imports: [LoaderComponent],
   templateUrl: './catalog.component.html',
   styleUrl: './catalog.component.css',
 })
@@ -12,9 +14,14 @@ export class CatalogComponent implements OnInit {
   private apiService = inject(ApiService);
   private destroyRef = inject(DestroyRef);
 
+  recipes: Recipe[] = [];
+  isFetching: boolean = false;
+
   ngOnInit(): void {
+    this.isFetching = true;
     const subscription = this.apiService.getRecipes().subscribe((recipes) => {
-      console.log(recipes);
+      this.recipes = recipes;
+      this.isFetching = false;
     });
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
