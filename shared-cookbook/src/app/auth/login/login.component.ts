3c240f62e1978a719753dc92e0ff3850a28c6b3d
AssiaIlieva <ac17@abv.bg>
2024-12-05 19:57:22 +0200

    Update login component

--- shared-cookbook/src/app/auth/login/login.component.ts
+++ shared-cookbook/src/app/auth/login/login.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+
+import { AuthApiService } from '../auth-api.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  userService = inject(AuthApiService);
+  error = signal<string>('');
+
+  onSubmit(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+
+    const { email, password } = form.value;
+    console.log(email, password);
+    this.userService.login(email, password).subscribe({
+      next: () => {
+        console.log('Login successful!');
+      },
+      error: (error: Error) => {
+        console.log(error);
+        this.error.set(error.message);
+      },
+    });
+  }
+}
