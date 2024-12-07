0b9ed01663fc055444c7d1e9ffff35500ccfb5c8
AssiaIlieva <ac17@abv.bg>
2024-12-07 10:45:53 +0200

    Update Home component to render the newest 4 recipes

--- shared-cookbook/src/app/home/home.component.ts
+++ shared-cookbook/src/app/home/home.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+
+import { RecipeCardComponent } from '../recipes/recipe-card/recipe-card.component';
+import { ApiRecipesService } from '../recipes/api.recipes.service';
+import { Recipe } from '../recipes/recipe.model';
+import { LoaderComponent } from '../shared/loader/loader.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RecipeCardComponent, LoaderComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  private apiService = inject(ApiRecipesService);
+  private destroyRef = inject(DestroyRef);
 
+  recipes = signal<Recipe[]>([]);
+  isFetching = signal<boolean>(false);
+  error = signal<string>('');
+
+  ngOnInit(): void {
+    this.isFetching.set(true);
+
+    const subscription = this.apiService.getLast4Recipes().subscribe({
+      next: (recipes) => {
+        this.recipes.set(recipes);
+      },
+      error: (error: Error) => {
+        console.log(error);
+        this.error.set(error.message);
+      },
+      complete: () => {
+        this.isFetching.set(false);
+      },
+    });
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }
