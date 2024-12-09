65166a38937848db4137911bfcae0ab87042498d
AssiaIlieva <ac17@abv.bg>
2024-12-09 08:27:41 +0200

    Create comments component and comments interfaces

---
+++ shared-cookbook/src/app/recipes/recipe-details/comments/comments.component.ts
@@ -0,0 +1,22 @@
+import {
+  Component,
+  DestroyRef,
+  EventEmitter,
+  inject,
+  input,
+  signal,
+} from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+import { Comment } from './comments.model';
+
+@Component({
+  selector: 'app-comments',
+  standalone: true,
+  imports: [DatePipe],
+  templateUrl: './comments.component.html',
+  styleUrl: './comments.component.css',
+})
+export class CommentsComponent {
+  comments = input.required<Comment[]>();
+}
